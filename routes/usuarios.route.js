5671cb2e9177641ac512dd53b2e9a3eb35c88d29
João Vitor Biston Nunes <se1100075@ESCOLA928.EDU>
2025-05-06 11:26:24 -0300

    Subindo rota de login

--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usuariosController = require("../controllers/usuarios.controller");
 
+router.post('/login', usuariosController.login);
 router.put('/:id', usuariosController.atualizarUsuario);
-router.post('/login', ()=>{console.log("Rota de Login")});
 router.post('/', usuariosController.cadastrarUsuario);
 
 module.exports = router;
