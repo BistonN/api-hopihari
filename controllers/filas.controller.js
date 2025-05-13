cd1fe159637de9ad39a2cb6e667f7f0ae6ec7a2a
João Vitor Biston Nunes <se1100075@ESCOLA928.EDU>
2025-05-13 11:22:31 -0300

    subindo rotas de filas

--- controllers/filas.controller.js
+++ controllers/filas.controller.js
@@ -1,11 +1,26 @@
 const mysql = require("../mysql");
 
+exports.verificarBrinquedo = async (req, res, next) => {
+    try {
+        const resultado = await mysql.execute(`
+            SELECT * FROM rides WHERE id = ?;
+            `, [req.params.idRide]);
 
-exports.entrarFila = async (req, res, next) => {
+        if (resultado.length == 0) {
+            return res.status(404).send({"Mensagem": "Brinquedo não Encontrado"});
+        }
+        next();
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+
+exports.entrarFila = async (req, res) => {
     try {
         const resultados = await mysql.execute(`
-                INSERT INTO lines (id_user, id_rides) VALUES(?, ?)
-            `, [res.locals.idUsuario, req.params.idRide]);
+                INSERT INTO hopi_hari_db.lines (id_user, id_ride) VALUES(?, ?)
+            `, [res.locals.idUsuario, Number(req.params.idRide)]);
         return res.status(201).send({"Mensagens": resultados});
     } catch (error) {
         return res.status(500).send(error)
