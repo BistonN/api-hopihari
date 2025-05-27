befacf8885a366de3bd9ed89d2f2521a4d5b9e32
João Vitor Biston Nunes <se1100075@ESCOLA928.EDU>
2025-05-27 11:19:50 -0300

    criando rota GET de brinquedos por area do parque

--- controllers/brinquedos.controller.js
+++ controllers/brinquedos.controller.js
@@ -19,3 +19,24 @@ exports.cadastrarBrinquedo = async (req, res) => {
         return res.status(500).send(error);
     }
 }
+
+exports.getBrinquedosByAreaName = async (req, res) => {
+    try {
+        resultados = await mysql.execute(`
+            SELECT * FROM rides WHERE id_areas = (
+                SELECT id FROM areas WHERE name = ?
+            );
+        `, [req.params.areaName]);
+
+        if (resultados.legth == 0) {
+            return res.status(404).send({"Mensagem": "Area do parque não Encontrada"});
+        }
+
+        return res.status(200).send({
+            "Mensagem": "Consulta realizada com Sucesso",
+            "resultados": resultados
+        })
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
--- routes/brinquedos.route.js
+++ routes/brinquedos.route.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const login = require("../middleware/usuarios.middleware");
 const brinquedosController = require('../controllers/brinquedos.controller');
 
-router.post('/', 
+router.post("/", 
     login.required,
     login.userRequired,
     brinquedosController.cadastrarBrinquedo
+);
+
+router.get("/area/:areaName", 
+    login.required, 
+    brinquedosController.getBrinquedosByAreaName
 );
 
 module.exports = router;
